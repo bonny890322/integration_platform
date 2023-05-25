16569e8a73db97a8cbc9c95fd6ef7f5f457eb01d
bonny890322 <bonny890322@gmail.com>
2023-05-25 22:13:11 +0800

    feat: 模型頁面list

--- src/app/views/features/model/model.component.ts
+++ src/app/views/features/model/model.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-model',
@@ -7,13 +8,41 @@ import { Component } from '@angular/core';
 })
 export class ModelComponent {
 
+  @ViewChild('dt') dt: Table
+
+  tableData: any[]
+
+  tableCols: any[];
+
   constructor(
   ) {
+    this.tableCols = [
+      { field: 'no', header: '' },
+      { field: 'name', header: '名稱' },
+      { field: 'description', header: '描述' },
+    ];
+
 
   }
 
   ngOnInit(): void {
+
   }
 
+  ngAfterContentInit(): void {
+    this.showTable()
+  }
+
+  showTable() {
+    this.tableData = [
+      { no: 1, name: '品質預測模型', description: '21個input,3個output' },
+      { no: 2, name: '品質預測模型2', description: '21個input,3個output' }
+    ];
+    console.log(this.tableData)
+  }
+
+  applyFilterGlobal($event: any, stringVal: any) {
+    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
 
 }
