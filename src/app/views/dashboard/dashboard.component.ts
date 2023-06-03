740f2d5e8a0b6e0dbcf15e1c7d43fb91655780f8
bonny890322 <bonny890322@gmail.com>
2023-06-04 00:18:11 +0800

    test: AWS deploy

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.API_URL;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  ngOnInit(): void {
+  }
+
 
 }
